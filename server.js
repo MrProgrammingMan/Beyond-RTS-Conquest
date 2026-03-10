ee57c09ea7b5a321974d56c7530dcd0bb1535370
MrProgrammingMan <sander.josefsson@edu.varberg.se>
2026-03-10 01:59:54 +0100

    further fixes to connection

--- server.js
+++ server.js
@@ -286,4 +286,4 @@ function pickEvent({ elapsed, p1hp, p2hp, midOwner }) {
 // ─── START ────────────────────────────────────────────────────────────────────
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`BKC server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`BRC server running on port ${PORT}`));
